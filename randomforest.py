4615e65d933b6ea90140861b247d24fe9787c3dc
Yasutomo Kawanishi <kawanishi@nagoya-u.jp>
2016-04-13 21:12:21 +0900

    SVMとRandomForestを更新

--- randomforest.py
+++ randomforest.py
@@ -1,12 +1,16 @@
-from sklearn.datasets import fetch_mldata
+# -*- coding: utf-8 -*-
+# from sklearn.datasets import fetch_mldata
+# from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
-import numpy as np
 
-mnist = fetch_mldata("MNIST original", data_home=".")
-data = np.asarray(mnist.data, np.float32)
-data_train, data_test, label_train, label_test = train_test_split(data, mnist.target, test_size=0.2)
+# 比較をしやすくするため，予めtrain/testを分けたものを読み込む
+from sklearn.externals import joblib
+data_train, data_test, label_train, label_test = joblib.load("mnist")
+
+# mnist = fetch_mldata("MNIST original", data_home=".")
+# data = np.asarray(mnist.data, np.float32)
+# data_train, data_test, label_train, label_test = train_test_split(data, mnist.target, test_size=0.2)
 
 classifier = RandomForestClassifier()
 classifier.fit(data_train, label_train)
--- svm.py
+++ svm.py
@@ -1,12 +1,18 @@
-from sklearn.datasets import fetch_mldata
-from sklearn.svm import LinearSVC
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix
-import numpy as np
+# -*- coding: utf-8 -*-
 
-mnist = fetch_mldata("MNIST original", data_home=".")
-data = np.asarray(mnist.data, np.float32)
-data_train, data_test, label_train, label_test = train_test_split(data, mnist.target, test_size=0.2)
+# from sklearn.datasets import fetch_mldata
+# from sklearn.cross_validation import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix
+
+# 比較をしやすくするため，予めtrain/testを分けたものを読み込む
+from sklearn.externals import joblib
+data_train, data_test, label_train, label_test = joblib.load("mnist")
+
+
+# mnist = fetch_mldata("MNIST original", data_home=".")
+# data = np.asarray(mnist.data, np.float32)
+# data_train, data_test, label_train, label_test = train_test_split(data, mnist.target, test_size=0.2)
 
 classifier = LinearSVC()
 classifier.fit(data_train, label_train)
