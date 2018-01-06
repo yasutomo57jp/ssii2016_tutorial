235f71e7fa356d8ba38724c418fb7308dfc9ecc1
Yasutomo KAWANISHI <kawanishi@is.nagoya-u.ac.jp>
2018-01-06 16:05:22 +0900

    時間の計測を追加

--- svm.py
+++ svm.py
@@ -3,7 +3,8 @@
 # from sklearn.datasets import fetch_mldata
 # from sklearn.cross_validation import train_test_split
 from sklearn.svm import LinearSVC
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+import time
 
 # 比較をしやすくするため，予めtrain/testを分けたものを読み込む
 from sklearn.externals import joblib
@@ -15,10 +16,17 @@ data_train, data_test, label_train, label_test = joblib.load("mnist")
 # data_train, data_test, label_train, label_test = train_test_split(data, mnist.target, test_size=0.2)
 
 classifier = LinearSVC()
+start = time.time()  # 処理時間の計測開始
 classifier.fit(data_train, label_train)
+training_time = time.time() - start
 
+start = time.time()  # 処理時間の計測開始
 result = classifier.predict(data_test)
+predict_time = time.time() - start
 
 # Confusion matrixを計算
+print(training_time, predict_time)
 cmat = confusion_matrix(label_test, result)
+acc = accuracy_score(label_test, result)
 print(cmat)
+print(acc)
